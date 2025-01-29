6ffbf094df6a31a723b4fbfb101c65089d77f5c7
kiarashAlizadeh <97706913+kiarashAlizadeh@users.noreply.github.com>
2025-01-29 18:06:45 +0330

    footer links changed

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -11,7 +11,8 @@ async function Footer() {
         <div class="footer-section">
           <h4>Quick Links</h4>
           <ul class="footer-links">
-            <li><a href="/home">Home</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/test#7775">Test Page</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/services">Services</a></li>
             <li><a href="/contact">Contact</a></li>
